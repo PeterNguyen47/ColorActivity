3044b84a12cf78e880431f62341d0475b17a7e59
PeterNguyen47 <70366885+PeterNguyen47@users.noreply.github.com>
2020-10-02 23:26:00 -0400

    Initial commit
    Assignment 3 - ColorActivity & ColorAdapter

---
+++ app/src/main/java/edu/temple/coloractivity/ColorActivity.java
@@ -0,0 +1,111 @@
+package edu.temple.coloractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+// 1. Create an app with activity ColorActivity as Main
+public class ColorActivity extends AppCompatActivity {
+
+    TextView bgTextView; // my text view background object
+    Spinner pnSpinner; // my spinner object
+    ArrayList<String> pnColors; // my Array List (Integer) object
+    ConstraintLayout pnLayout; // my layout
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // reference objects by R.id
+        pnSpinner = findViewById(R.id.pnSpinner);
+        bgTextView = findViewById(R.id.bgTextView);
+        pnLayout = (ConstraintLayout)findViewById(R.id.layout);
+        this.context = getApplicationContext();
+
+        // make sure views are found in log, if not found, will display false
+        Log.d("ColorActivity", "pnSpinner was found " + (pnSpinner != null));
+        Log.d("ColorActivity", "bgTextView was found " + (bgTextView != null));
+
+        // make array list of 10 string of colors, data set
+        pnColors = new ArrayList<>();
+        pnColors.add("Choose a Color");
+        pnColors.add("Green");
+        pnColors.add("Blue");
+        pnColors.add("Black");
+        pnColors.add("Red");
+        pnColors.add("Dark Gray");
+        pnColors.add("Gray");
+        pnColors.add("Yellow");
+        pnColors.add("Magenta");
+        pnColors.add("Cyan");
+        pnColors.add("Light Gray");
+
+        final BaseAdapter pnAdapter = new ColorAdapter(this, pnColors);
+        pnSpinner.setAdapter(pnAdapter);
+        pnLayout = findViewById(R.id.layout);
+
+        pnSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // log follows above line as reference
+                Log.d("ColorActivity", "new item selected... position = " + position + "... id = " + id);
+
+                // getting item at position as a string, parent is getting item from adapter (Low coupling)
+                bgTextView.setText(parent.getItemAtPosition(position).toString());
+                bgTextView.setTextColor(Integer.parseInt(String.valueOf(position)));
+
+                Log.d("ColorActivity","back ground Color list is functional");
+                if (pnSpinner.getSelectedItem().toString().equals("Choose a Color")) {
+                    bgTextView.setBackgroundColor(Color.WHITE);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Green")) {
+                    bgTextView.setBackgroundColor(Color.GREEN); //bg turns green
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Blue")) {
+                    bgTextView.setBackgroundColor(Color.BLUE);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Black")) {
+                    bgTextView.setBackgroundColor(Color.BLACK);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Red")) {
+                    bgTextView.setBackgroundColor(Color.RED);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Dark Gray")) {
+                    bgTextView.setBackgroundColor(Color.DKGRAY);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Gray")) {
+                    bgTextView.setBackgroundColor(Color.GRAY);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Yellow")) {
+                    bgTextView.setBackgroundColor(Color.YELLOW);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Magenta")) {
+                    bgTextView.setBackgroundColor(Color.MAGENTA);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Cyan")) {
+                    bgTextView.setBackgroundColor(Color.CYAN);
+                }
+                else if (pnSpinner.getSelectedItem().toString().equals("Light Gray")) {
+                    bgTextView.setBackgroundColor(Color.LTGRAY);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/temple/coloractivity/ColorAdapter.java
@@ -0,0 +1,106 @@
+package edu.temple.coloractivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+// 2. Created ColorAdapter that extends BaseAdapter to present set of color options
+public class ColorAdapter extends BaseAdapter {
+
+    Context context; //context object for adapter reference
+    ArrayList<String> pnColors; // array of strings object
+    Spinner pnSpinner;
+    // Ex. ArrayList<String> PN <- just an independent object from same class, can be any name
+
+    // constructor
+    public ColorAdapter(Context colorActivity, ArrayList<String> pnColors) {
+
+        context = colorActivity; // THIS only needed when instance variables have same name
+        this.pnColors = pnColors; // sent over from main activity using context color activity
+        this.pnSpinner = pnSpinner;
+    }
+
+    // counts how many in data set by size because ArrayList (String uses length)
+    @Override
+    public int getCount() {
+        Log.d("ColorActivity", "pnColors size is: " + pnColors.size());
+        return pnColors.size();
+    }
+
+    // Give item found at this position, returns string
+    @Override
+    public Object getItem(int position) {
+        // follow flow
+        Log.d("ColorActivity", "position is: " + position); // position is "1"
+        Log.d("ColorActivity", "pnColors position is: " + pnColors.get(position)); // color at position "Tabby cat"
+        return pnColors.get(position); // checking 2 values, returns position of color in spinner
+    }
+
+    // left alone
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // where user sees initial display
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // How we want things to display to the user, default is selected for user
+        Log.d("ColorActivity", "view position is: " + position);
+
+        int pnColors[] = new int[11];
+        pnColors[0] = Color.BLACK;
+        pnColors[1] = Color.GREEN;
+        pnColors[2] = Color.BLUE;
+        pnColors[3] = Color.BLACK;
+        pnColors[4] = Color.RED;
+        pnColors[5] = Color.DKGRAY;
+        pnColors[6] = Color.GRAY;
+        pnColors[7] = Color.YELLOW;
+        pnColors[8] = Color.MAGENTA;
+        pnColors[9] = Color.CYAN;
+        pnColors[10] = Color.LTGRAY;
+
+        TextView bgTextView;
+        if (convertView == null) {
+            bgTextView = new TextView(context);
+            bgTextView.setPadding(5, 5, 5, 5); // spacing in spinner (optional)
+            bgTextView.setTextSize(18);
+            bgTextView.setBackgroundColor(pnColors[position % pnColors.length]); // spinner bg color
+        }
+        else
+            bgTextView = (TextView) convertView;
+
+            bgTextView.setText(getItem(position).toString()); // get text from position
+            bgTextView.setBackgroundColor(Color.WHITE); // spinner bg (WHITE)
+            bgTextView.setTextColor(pnColors[position % pnColors.length]);
+
+
+        return bgTextView;
+    }
+
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        Log.d("ColorActivity", "dropdown view position is: " + position);
+        // TODO convert position to color string
+        // TODO convert color string to color value
+        // TODO set background text view to color value
+        View v = getView(position, convertView, parent);
+
+        return v;
+    }
+}
